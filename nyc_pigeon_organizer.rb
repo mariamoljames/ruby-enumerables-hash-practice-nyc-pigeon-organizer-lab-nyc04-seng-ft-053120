e9ec6ee1a436712bbdb3955fd76a6fd34d8d6964
Mariamol James <mariamol.james@gmail.com>
2020-06-11 16:06:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organizer={}
+  names=data.values[0].values.flatten.uniq
+  attributes=data.keys
+  count=0
+  while count<names.length do
+    inner_count=0
+    while inner_count<attributes.length do
+      organizer[names[count]]=attributes[inner_count]
+      inner_count+=1
+    end
+    count+=1
+  end
+  binding.pry
+  organizer
 end
